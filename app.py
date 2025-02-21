2e2022abd26488d06f2c1a3bd6d609098dfac778
AquibMolla <111010942+AquibMolla@users.noreply.github.com>
2025-02-21 14:38:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,29 +71,29 @@ def simulate_forager(max_energy, laziness):
     
     anim = FuncAnimation(fig, update, frames=len(path), init_func=init, blit=False, interval=50)
     plt.close(fig)
-    print("Hi!!!")
+
     return anim.to_jshtml(), len(path)-1, int((width * height) - np.sum(space))
 
 def main():
     st.title("Inertial Forager Simulation")
     max_energy = st.slider("Full energy (1-20):", 1, 20, 10)
     laziness = st.slider("Laziness (0-1):", 0.0, 1.0, 0.5)
-    #if st.button("Run Simulation"):
-    #    result = simulate_forager(max_energy, laziness)
-    #    with st.spinner('Simulating...'):
-    #        html, lifetime, eaten = result #simulate_forager(max_energy, laziness)
-    #    st.write(f"Lifetime: {lifetime} steps | Cabbages eaten: {eaten}")
-    #    st.components.v1.html(html, height=600, scrolling=True)
     if st.button("Run Simulation"):
+        result = simulate_forager(max_energy, laziness)
         with st.spinner('Simulating...'):
-            result = simulate_forager(max_energy, laziness)
-        
-        if result is not None and all(val is not None for val in result):
-            html, lifetime, eaten = result
-            st.write(f"Lifetime: {lifetime} steps | Cabbages eaten: {eaten}")
-            st.components.v1.html(html, height=600, scrolling=True)
-        else:
-            st.error("Simulation failed. Please try again.")
+            html, lifetime, eaten = result #simulate_forager(max_energy, laziness)
+        st.write(f"Lifetime: {lifetime} steps | Cabbages eaten: {eaten}")
+        st.components.v1.html(html, height=600, scrolling=True)
+    #if st.button("Run Simulation"):
+    #    with st.spinner('Simulating...'):
+    #        result = simulate_forager(max_energy, laziness)
+    #    
+    #    if result is not None and all(val is not None for val in result):
+    #        html, lifetime, eaten = result
+    #        st.write(f"Lifetime: {lifetime} steps | Cabbages eaten: {eaten}")
+    #        st.components.v1.html(html, height=600, scrolling=True)
+    #    else:
+    #        st.error("Simulation failed. Please try again.")
 
 if __name__ == "__main__":
     main()
