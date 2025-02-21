dc4f26f72d78e810f04eb4c25bc4be06506a41dc
AquibMolla <111010942+AquibMolla@users.noreply.github.com>
2025-02-21 15:29:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import random
 import streamlit as st
 
 def initialize_simulation(width, height):
-    space = np.ones((width+1, height+1))
-    food_sites = [(x, y) for x in range(width+1) for y in range(height+1)]
+    space = np.ones((width, height))
+    food_sites = [(x, y) for x in range(width) for y in range(height)]
     return space, food_sites
 
 def move_forager(x, y, energy, is_resting, space, width, height, max_energy, p):
@@ -30,7 +30,7 @@ def move_forager(x, y, energy, is_resting, space, width, height, max_energy, p):
 
 def simulate_forager(max_energy, laziness):
     width = height = 4 * max_energy
-    space, food_sites = initialize_simulation(width+1, height+1)
+    space, food_sites = initialize_simulation(width, height)
     x, y = width//2, height//2
     energy, is_resting, is_dead = max_energy, True, False
     path = [(x, y)]
