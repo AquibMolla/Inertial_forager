390c6e8e7586f1c35d8d565bca9bf6d7dc976e69
AquibMolla <111010942+AquibMolla@users.noreply.github.com>
2025-02-21 14:14:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def simulate_forager(max_energy, laziness):
         energy_history.append(energy)
         food_history.append([(i, j) for i in range(width) for j in range(height) if space[i, j] == 1])
     
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
     def init():
         ax1.clear(), ax2.clear()
         ax1.set(xlim=(0, width-1), ylim=(0, height-1), title='Forager Movement')
